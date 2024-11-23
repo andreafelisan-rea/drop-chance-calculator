6b55052593231319f269012611fb29a5692e35a7
Xandrea_san <ayezaavriellekim@gmail.com>
2024-11-23 12:53:59 +0800

    Create drop chance calculator main.cpp

---
+++ drop chance calculator main.cpp
@@ -0,0 +1,8 @@
+#include "function.h"
+#include "function.h"
+
+double chance = calculatorDropChance(dropRate,attempts);
+ double chance -1- pow(1 - ( 1 - dropRate , attempts) ;
+
+return chance;
+}
