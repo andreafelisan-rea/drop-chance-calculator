27922f66844a7b2db159f961990a58167307e273
Xandrea_san <ayezaavriellekim@gmail.com>
2024-11-25 18:22:53 +0800

    Update drop chance calculator main.cpp

--- drop chance calculator main.cpp
+++ drop chance calculator main.cpp
@@ -1,5 +1,5 @@
-#include "function.h"
-#include "function.h"
+#include "header1.h"
+#include "header2.h"
 
 double chance = calculatorDropChance(dropRate,attempts);
  double chance -1- pow(1 - ( 1 - dropRate , attempts) ;
